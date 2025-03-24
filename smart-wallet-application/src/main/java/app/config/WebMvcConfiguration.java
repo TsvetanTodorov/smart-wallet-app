4ee1cd024f55ec717432c2b961a07dca7219a9f5
Tsvetan <tstodorofff@gmail.com>
2025-03-24 17:15:55 +0200

    Deleting SessionCheckInterceptor

--- smart-wallet-application/src/main/java/app/config/WebMvcConfiguration.java
+++ smart-wallet-application/src/main/java/app/config/WebMvcConfiguration.java
@@ -1,29 +1,15 @@
 package app.config;
 
-import app.security.SessionCheckInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
-
-    @Autowired
-    private SessionCheckInterceptor interceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-
-        registry.addInterceptor(interceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/css/**", "/images/**");
-    }
-
 
     @Bean
     public SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -33,9 +19,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
                         .requestMatchers("/","/register").permitAll()
                         .anyRequest().authenticated())
 
-                .formLogin(form -> form
+                        .formLogin(form -> form
                         .loginPage("/login")
-//                                .usernameParameter("username")  we have them by default, except if we don't use username but email instead
+//                have these parameters by default, except if we don't use username but email instead
+//                                .usernameParameter("username")
 //                                .passwordParameter("password")
                         .defaultSuccessUrl("/home")
                         .failureUrl("/login?error")
