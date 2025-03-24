235b9aae2285320d5d3142d81bcb71b4e39f9749
Tsvetan <tstodorofff@gmail.com>
2025-03-24 16:40:31 +0200

    Adding spring-security-dependency

--- smart-wallet-application/src/main/java/app/config/WebMvcConfiguration.java
+++ smart-wallet-application/src/main/java/app/config/WebMvcConfiguration.java
@@ -2,7 +2,11 @@ package app.config;
 
 import app.security.SessionCheckInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,4 +23,25 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/images/**");
     }
+
+
+    @Bean
+    public SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(matchers -> matchers
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/","/register").permitAll()
+                        .anyRequest().authenticated())
+
+                .formLogin(form -> form
+                        .loginPage("/login")
+//                                .usernameParameter("username")  we have them by default, except if we don't use username but email instead
+//                                .passwordParameter("password")
+                        .defaultSuccessUrl("/home")
+                        .failureUrl("/login?error")
+                        .permitAll());
+
+        return http.build();
+    }
+
 }
