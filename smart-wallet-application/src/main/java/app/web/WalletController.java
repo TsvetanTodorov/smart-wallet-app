c7e3c72a2d454fc8210278f4ed19034a796eda61
Tsvetan <tstodorofff@gmail.com>
2025-03-25 17:45:09 +0200

    Implementing getWalletsPage and createNewWallet methods

--- smart-wallet-application/src/main/java/app/web/WalletController.java
+++ smart-wallet-application/src/main/java/app/web/WalletController.java
@@ -1,26 +1,50 @@
 package app.web;
 
+import app.security.AuthenticationMetadata;
+import app.user.model.User;
+import app.user.service.UserService;
 import app.wallet.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/wallets")
 public class WalletController {
 
     private final WalletService walletService;
+    private final UserService userService;
 
     @Autowired
-    public WalletController(WalletService walletService) {
+    public WalletController(WalletService walletService, UserService userService) {
         this.walletService = walletService;
+        this.userService = userService;
     }
 
     @GetMapping
-    public String getWalletsPage(){
+    public ModelAndView getWalletsPage(@AuthenticationPrincipal AuthenticationMetadata authenticationMetadata) {
 
-        return "wallets";
+        User user = userService.getById(authenticationMetadata.getUserId());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("wallets");
+        modelAndView.addObject("user", user);
+
+        return modelAndView;
+    }
+
+    @PostMapping
+    public String createNewWallet(@AuthenticationPrincipal AuthenticationMetadata authenticationMetadata) {
+
+        User user = userService.getById(authenticationMetadata.getUserId());
+        walletService.unlockNewWallet(user);
+
+        return "redirect:/wallets";
     }
 
 }
