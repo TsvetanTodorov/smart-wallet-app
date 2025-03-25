1cbeb645cfff2db5b20fe8ac2ac428529f3966a8
Tsvetan <tstodorofff@gmail.com>
2025-03-25 17:44:31 +0200

    Refactoring getAllTransactions and getTransactionById

--- smart-wallet-application/src/main/java/app/web/TransactionController.java
+++ smart-wallet-application/src/main/java/app/web/TransactionController.java
@@ -1,10 +1,14 @@
 package app.web;
 
+import app.security.AuthenticationMetadata;
 import app.transaction.model.Transaction;
 import app.transaction.service.TransactionService;
 
+import app.user.model.User;
+import app.user.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,35 +24,40 @@ import java.util.UUID;
 public class TransactionController {
 
     private final TransactionService transactionService;
+    private final UserService userService;
 
     @Autowired
-    public TransactionController(TransactionService transactionService) {
+    public TransactionController(TransactionService transactionService, UserService userService) {
 
         this.transactionService = transactionService;
+        this.userService = userService;
     }
 
     @GetMapping
-    public ModelAndView getAllTransactions(HttpSession session) {
+    public ModelAndView getAllTransactions(@AuthenticationPrincipal AuthenticationMetadata authenticationMetadata ) {
 
-        UUID userId = (UUID) session.getAttribute("user_id");
-        List<Transaction> transactions = transactionService.getAllByOwnerId(userId);
+        User user = userService.getById(authenticationMetadata.getUserId());
+        List<Transaction> transactions = transactionService.getAllByOwnerId(authenticationMetadata.getUserId());
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("transactions");
         modelAndView.addObject("transactions", transactions);
+        modelAndView.addObject("user", user);
 
         return modelAndView;
 
     }
 
     @GetMapping("/{id}")
-    public ModelAndView getTransactionById(@PathVariable UUID id) {
+    public ModelAndView getTransactionById(@PathVariable UUID id, @AuthenticationPrincipal AuthenticationMetadata authenticationMetadata) {
 
+        User user = userService.getById(authenticationMetadata.getUserId());
         Transaction transaction = transactionService.getById(id);
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("transaction-result");
         modelAndView.addObject("transaction", transaction);
+        modelAndView.addObject("user", user);
 
         return modelAndView;
     }
